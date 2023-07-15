72b4a6fd9bcf86bd7cdf9502d6cd7dc54dd41bb3
hamosaeed33 <hamosaeed33@gmail.com>
2023-07-14 19:43:23 -0700

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Hello
+ *
+ * Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+	putcher(ch);
+	ch++;
+}
+	putcher('\n');
+return (0);
+}
